ebf64e8c571c9ccca0fbb3b1ccdb43595161a380
Sergo6788 <fedyakov2006.sergey@gmail.com>
2024-10-17 15:37:40 +0300

    checking password and email

--- app/src/main/java/com/example/mudisapp/fragment/LoginFragment.java
+++ app/src/main/java/com/example/mudisapp/fragment/LoginFragment.java
@@ -7,12 +7,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.mudisapp.R;
 import com.example.mudisapp.databinding.FragmentLoginBinding;
+
+import java.net.PasswordAuthentication;
 
 
 public class LoginFragment extends Fragment {
@@ -36,5 +40,25 @@ public class LoginFragment extends Fragment {
         binding.tvButtonSignUp.setOnClickListener(v -> {
             Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_registerFragment);
         });
+        binding.btSignIn.setOnClickListener(v -> {
+            checkEnterData();
+        });
+        binding.tvButtonForgot.setOnClickListener(v ->{
+
+        });
+
+    }
+    private void checkEnterData(){
+        if(!Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.getText()).matches()){
+            Toast.makeText(requireContext(), "Email Incorrect", Toast.LENGTH_SHORT).show();
+        } else if (binding.etPassword.getText().toString().length()<6 || !binding.etPassword.getText().toString().contains(" ")) {
+            Toast.makeText(requireContext(), "Password length must be more than 6 characters and without space", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            allCorrect();
+        }
+    }
+    private void allCorrect(){
+
     }
 }
