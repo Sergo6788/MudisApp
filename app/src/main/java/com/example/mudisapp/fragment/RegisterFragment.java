0f3d49827a4df1f64949bbfcfdb61e1dc9350ac4
xandea1 <dantiv45@gmail.com>
2024-10-17 00:56:11 -0400

    Register fragment with error

--- app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
@@ -2,7 +2,10 @@ package com.example.mudisapp.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +15,25 @@ import com.example.mudisapp.R;
 
 
 public class RegisterFragment extends Fragment {
-
+    private FragmentRegisterBinding binding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater);
+
+        return binding.getRoot();
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        applyClick();
+    }
+    private void applyClick(){
+        binding.tvSignin.setOnClickListener(v -> {
+           Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_loginFragment);
+      })
+   }
 }
