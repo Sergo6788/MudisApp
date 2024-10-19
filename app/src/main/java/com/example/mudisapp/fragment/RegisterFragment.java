453e0046e385374ebb875f24f6246a5a05c3b125
xandea1 <dantiv45@gmail.com>
2024-10-18 22:07:46 -0400

    Error with FindViewById

--- app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
@@ -11,39 +11,55 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.mudisapp.R;
 import com.example.mudisapp.databinding.FragmentRegisterBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class RegisterFragment extends Fragment {
     private FragmentRegisterBinding binding;
+    TextInputEditText textInputEditTextEmail, textInputEditTextPassword, textInputEditTextRepeatPassword;
+    String email, password, repeatPassword;
+    ImageView eyeClosed, eyeOpened;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater);
-
         return binding.getRoot();
     }
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+        textInputEditTextEmail = findViewById(R.id.etEmail);
+        textInputEditTextPassword = findViewById(R.id.etPassword);
+        textInputEditTextRepeatPassword = findViewById(R.id.etPasswordRepeat);
+        eyeClosed = findViewById(R.id.ivEyeClosed);
+        eyeOpened = findViewById(R.id.ivEyeOpened);
         applyClick();
     }
+
     private void applyClick(){
+        textInputEditTextRepeatPassword.setVisibility(View.INVISIBLE);
+
         binding.tvSignin.setOnClickListener(v -> {
            Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_loginFragment);
       });
         binding.btSignUp.setOnClickListener(v -> {
 
         });
-        binding.ivEyeClosed.setOnClickListener(v -> {
 
-        });
+   }
+
+   private void checkClickEye(){
+        if(binding.ivEyeClosed.setOnClickListener(v -> {
+
+       });
    }
 
    private void checkEnterData(){
@@ -54,4 +70,5 @@ public class RegisterFragment extends Fragment {
 
         }
     }
+
 }
