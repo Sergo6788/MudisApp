85c6796b0b18df7c1b148a731a2632246c06e99e
xandea1 <dantiv45@gmail.com>
2024-10-21 23:19:42 -0400

    Ended Firebase registration, cant fix 1 problem with passwords

--- app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/mudisapp/fragment/RegisterFragment.java
@@ -2,6 +2,8 @@ package com.example.mudisapp.fragment;
 
 import static androidx.appcompat.content.res.AppCompatResources.getDrawable;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,13 +21,20 @@ import android.widget.Toast;
 
 import com.example.mudisapp.R;
 import com.example.mudisapp.databinding.FragmentRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class RegisterFragment extends Fragment {
     private FragmentRegisterBinding binding;
     private boolean isPasswordHidden = true;
     private boolean isRepeatPasswordHidden = true;
+    private FirebaseAuth mAuth;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,28 +44,28 @@ public class RegisterFragment extends Fragment {
 
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         applyClick();
     }
 
-    private void applyClick(){
+    private void applyClick() {
         binding.tvSignin.setOnClickListener(v -> {
-           Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_loginFragment);
-      });
+            Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_loginFragment);
+        });
         binding.btSignUp.setOnClickListener(v -> {
-
+            createUser();
         });
 
         binding.ivEyePassword.setOnClickListener(v -> {
 
-            if(isPasswordHidden){
+            if (isPasswordHidden) {
                 binding.ivEyePassword.setImageDrawable(requireContext().getDrawable(R.drawable.eye_opened_img_svg));
 
                 binding.etPassword.setTransformationMethod(null);
 
-            }
-            else{
+            } else {
                 binding.ivEyePassword.setImageDrawable(requireContext().getDrawable(R.drawable.eye_closed_img_svg));
 
                 binding.etPassword.setTransformationMethod(new PasswordTransformationMethod());
@@ -67,13 +76,12 @@ public class RegisterFragment extends Fragment {
         });
         binding.ivEyeRepeatPassword.setOnClickListener(v -> {
 
-            if(isRepeatPasswordHidden){
+            if (isRepeatPasswordHidden) {
                 binding.ivEyeRepeatPassword.setImageDrawable(requireContext().getDrawable(R.drawable.eye_opened_img_svg));
 
                 binding.etPasswordRepeat.setTransformationMethod(null);
 
-            }
-            else{
+            } else {
                 binding.ivEyeRepeatPassword.setImageDrawable(requireContext().getDrawable(R.drawable.eye_closed_img_svg));
 
                 binding.etPasswordRepeat.setTransformationMethod(new PasswordTransformationMethod());
@@ -82,26 +90,51 @@ public class RegisterFragment extends Fragment {
 
 
         });
-   }
+    }
 
 
-   private void checkEnterData(){
+    private void checkEnterData() {
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.getText()).matches())
-        {
+        if (!Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.getText()).matches()) {
             Toast.makeText(requireContext(), "Email Incorrect", Toast.LENGTH_SHORT).show();
-        }
-        else if (binding.etPassword.getText().toString().length()<6 || !binding.etPassword.getText().toString().contains(" "))
-        {
+        } else if (binding.etPassword.getText().toString().length() < 6 || !binding.etPassword.getText().toString().contains(" ")) {
             Toast.makeText(requireContext(), "Password length must be more than 6 symbols and without spaces", Toast.LENGTH_SHORT).show();
-        }
+        } else if(binding.etPassword != binding.etPasswordRepeat)
+            Toast.makeText(requireContext(), "Password must be the same", Toast.LENGTH_SHORT).show();
         else
             allCorect();
     }
-
 
     private void allCorect(){
 
     }
 
+    private void createUser(){
+        String email = binding.etEmail.getText().toString();
+        String password = binding.etPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task){
+                if(task.isSuccessful()) {
+                    Toast.makeText(requireContext(), "User registration successfully", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }
+                else {
+                    Toast.makeText(requireContext(), "User registration failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
