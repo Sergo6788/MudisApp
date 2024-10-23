666236212cfdd40d28d4a2fe2d2bee7978668f57
xandea1 <dantiv45@gmail.com>
2024-10-22 21:54:09 -0400

    Ended shared but with errors

---
+++ app/src/main/java/com/example/mudisapp/app/App.java
@@ -0,0 +1,18 @@
+package com.example.mudisapp.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.mudisapp.shared.SharedManager;
+
+public class App {
+    public static SharedManager sharedManager;
+    public static Context appContext;
+
+
+    public void onCreate(){
+        super.onCreate();
+        appContext = appContext.getApplicationContext();
+        sharedManager = new SharedManager(getBaseContext());
+    }
+}
---
+++ app/src/main/java/com/example/mudisapp/shared/SharedManager.java
@@ -0,0 +1,20 @@
+package com.example.mudisapp.shared;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedManager {
+    public SharedManager(Context baseContext){
+        this.sharedPreferences = baseContext.getSharedPreferences("MudisApp", Context.MODE_PRIVATE);
+    }
+    private SharedPreferences sharedPreferences;
+
+    public boolean isUserAuthorized(){
+        return sharedPreferences.getBoolean("isUserAuthorized", false);
+    }
+    public void userAuthorize(){
+        sharedPreferences.edit().putBoolean("isUserAuthorized", true).apply();
+    }
+    public void userLogout(){
+        sharedPreferences.edit().putBoolean("isUserAuthorized", false).apply();
+    }
+}
