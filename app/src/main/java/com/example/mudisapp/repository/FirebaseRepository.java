0904d831661fef27111da16300677b074d884bde
xandea1 <dantiv45@gmail.com>
2024-11-24 00:28:22 +0200

    Started rebuilding Menu

--- app/src/main/java/com/example/mudisapp/repository/FirebaseRepository.java
+++ app/src/main/java/com/example/mudisapp/repository/FirebaseRepository.java
@@ -2,6 +2,9 @@ package com.example.mudisapp.repository;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
 import com.example.mudisapp.model.MenuModel;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
@@ -15,29 +18,28 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
 
-public class FirebaseRepository {
-    private FirebaseFirestore dataBase;
-    private Gson gson;
-    public FirebaseRepository(FirebaseFirestore dataBase){
-        this.dataBase = dataBase;
-        this.gson = new Gson();
+public class FirebaseRepository extends ViewModel {
+    private FirebaseFirestore dataBase = FirebaseFirestore.getInstance();
+    private ArrayList<MenuModel> menuList = new ArrayList<>();
+    public MutableLiveData<Boolean> isTaskReady = new MutableLiveData<>(false);
+
+
+    public void getMenu() {
+        dataBase.collection("Dishes").get()
+                .addOnSuccessListener(task -> {
+                    for (DocumentSnapshot documentSnapshot : task.getDocuments()) {
+                        MenuModel data = documentSnapshot.toObject(MenuModel.class);
+                        menuList.add(data);
+                    }
+                    isTaskReady.setValue(true);
+                })
+                .addOnFailureListener(error -> {
+                    Log.d("ERROR", error.getMessage());
+                });
     }
-
-
-    public ArrayList<MenuModel> getMenu(){
-        Task<QuerySnapshot> query = dataBase.collection("Dishes").get();
-        ArrayList<MenuModel> list = new ArrayList<>();
-        try {
-            QuerySnapshot snapshot = query.getResult();
-            for(DocumentSnapshot documentSnapshot: snapshot.getDocuments()){
-                MenuModel data = documentSnapshot.toObject(MenuModel.class);
-                list.add(data);
-            }
-        }
-        catch (Exception e){
-            Log.d("ERROR", e.getMessage().toString());
-        }
-        return list;
+    public ArrayList<MenuModel> getMenuList(){
+        isTaskReady.setValue(false);
+        return menuList;
     }
 
 }
